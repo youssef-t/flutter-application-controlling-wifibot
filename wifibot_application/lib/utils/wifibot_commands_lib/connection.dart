f1dd681b5bfd0dcb0967af314ba471b1f1574717
youssef-t <yousseftaleb1999@gmail.com>
2021-10-21 09:30:19 +0200

    [ADDED] Connecting via TCP

--- wifibot_application/lib/utils/wifibot_commands_lib/connection.dart
+++ wifibot_application/lib/utils/wifibot_commands_lib/connection.dart
@@ -1,5 +1,21 @@
 
+import 'package:wifibot_application/utils/wifibot_commands_lib/commands.dart';
+import 'package:wifibot_application/utils/wifibot_commands_lib/data_wifibot.dart';
+
 /// Manage the connection with the wifibot
 class Connection{
   Connection();
+
+  int? _rotationCameraX;
+  int? _rotationCameraY;
+
+  Commands? _command;
+  DataWifibot? _dataWifibot;
+  final int _timer = 100; //100ms
+
+
+  void setSpeed(int speedLeft, int speedRight){ }
+
+  void setAction() { }
+
 }
--- wifibot_application/lib/utils/wifibot_commands_lib/connection_tcp.dart
+++ wifibot_application/lib/utils/wifibot_commands_lib/connection_tcp.dart
@@ -1,5 +1,48 @@
 
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:wifibot_application/utils/wifibot_commands_lib/commands.dart';
+import 'package:wifibot_application/utils/wifibot_commands_lib/data_wifibot.dart';
+
 ///  Class to manage the TCP connection  with the Wifibot
 class ConnectionTCP {
+
+  Socket? _socketWifiBot;
+
+  //TODO change the IP address
+  dynamic _wifiBotIPAddress = "127.0.0.1";
+  final int _tcpPortWifibot = 15020;
+
   ConnectionTCP();
+
+  Future<void> connect() async {
+    try {
+      _socketWifiBot = await Socket.connect(_wifiBotIPAddress, _tcpPortWifibot);
+    } on SocketException catch (e) {
+      print('SocketException: $e');
+    } catch(e) {
+      print('Exception when trying to connect: $e');
+    }
+      print('Connected');
+    }
+
+
+  void send(String commandString) {
+    _socketWifiBot?.add(utf8.encode('hello'));
+  }
+
+  void receive(DataWifibot dataWifibot){
+
+  }
+
+  void run() {
+
+  }
+
+  void disconnect(){
+    _socketWifiBot?.close();
+  }
+
+
 }
