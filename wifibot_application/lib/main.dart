8ad31a3dff831fc7ef474665327f6e79f3217119
youssef-t <yousseftaleb1999@gmail.com>
2021-10-28 14:29:21 +0200

    [ADDED - not functionnal] Initial UI with a button for the controller and another one for settings

--- wifibot_application/lib/main.dart
+++ wifibot_application/lib/main.dart
@@ -4,21 +4,75 @@
 
 import 'package:flutter/material.dart';
 
+// Variable to store the width of the screen
+double? screenWidth;
+// Variable to store the height of the screen
+double? screenHeight;
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Welcome to the Wifibot controller App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to the Wifibot controller App'),
+      title: 'Wifibot Controller',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const fontSizeButton = 30.0;
+
+
+  static const textButtonStyle = TextStyle(
+    fontSize: fontSizeButton,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the height and the width of the screen
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+
+    print("screenHeight : $screenHeight");
+    print("screenWidth: $screenWidth");
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          "Wifibot Controller",
         ),
-        body: const Center(
-          child: Text('WIFIBOT'),
+        ),
+        actions: const [
+          IconButton(onPressed: null, icon: Icon(Icons.settings, color: Colors.black,) )
+        ],
+      ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 50.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(double.infinity, 50), // double.infinity is the width and 30 is the height
+            ),
+            child: Text("Controller", style: textButtonStyle, ),
+
+
+          ),
         ),
       ),
     );
